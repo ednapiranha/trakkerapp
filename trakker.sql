81177a1a818c35d754b696ce78776ef723118e16
Jen Fong-Adwent <jen@ednapiranha.com>
2013-04-01 16:10:10 -0400

    updated sql

--- trakker.sql
+++ trakker.sql
@@ -1,16 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4004
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: 127.0.0.1 (MySQL 5.5.19)
-# Database: trakker_test
-# Generation Time: 2013-04-01 20:07:53 +0000
-# ************************************************************
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
